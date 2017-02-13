14be87b1be593c91c988291fd3ff4134b810149c
R. Hill <rhill@uw.edu>
2017-02-12 18:46:56 -0800

    Working lab5)

--- labs/lab5/webapp/static/js/lab5.js
+++ labs/lab5/webapp/static/js/lab5.js
@@ -33,42 +33,33 @@ $(document).ready(function() {
   }
 
   /* update the LEDs based on their SSE state monitor */
-  function updateLeds(ledNum,ledValue) {
-    // console.log("Inside updateLeds");
+  function updateLeds(ledNum, ledValue) {
     if (ledNum === 1) {
       if (ledValue === '1') {
-        // $('#red_led_label').text('ON');
         led1 = "ON"
         $('#red_led_label').toggleClass('label-default', false);
         $('#red_led_label').toggleClass('label-danger', true);
       } else if (ledValue === '0') {
-        // $('#red_led_label').text('OFF');
         led1 = "OFF"
         $('#red_led_label').toggleClass('label-default', true);
         $('#red_led_label').toggleClass('label-danger', false);
       }
-    }
-    else if (ledNum === 2) {
+    } else if (ledNum === 2) {
       if (ledValue === '1') {
-        // $('#grn_led_label').text('ON');
         led2 = "ON"
         $('#grn_led_label').toggleClass('label-default', false);
         $('#grn_led_label').toggleClass('label-success', true);
       } else if (ledValue === '0') {
-        // $('#grn_led_label').text('OFF');
         led2 = "OFF"
         $('#grn_led_label').toggleClass('label-default', true);
         $('#grn_led_label').toggleClass('label-success', false);
       }
-    }
-    else if (ledNum === 3) {
+    } else if (ledNum === 3) {
       if (ledValue === '1') {
-        // $('#blu_led_label').text('ON');
         led3 = "ON"
         $('#blu_led_label').toggleClass('label-default', false);
         $('#blu_led_label').toggleClass('label-primary', true);
       } else if (ledValue === '0') {
-        // $('#blu_led_label').text('OFF');
         led3 = "OFF"
         $('#blu_led_label').toggleClass('label-default', true);
         $('#blu_led_label').toggleClass('label-primary', false);
@@ -76,85 +67,153 @@ $(document).ready(function() {
     }
   }
 
-    // The red button click functions run asynchronously in the browser
-    $('#red_led_btn').click(function() {
-      if (led1 === "OFF") {led1 = "ON";} else {led1 = "OFF";}
-      var params = 'led=1&state='+led1;
-      console.log('Led Command with params:' + params);
-      $.post('/ledcmd', params, function(data, status){
-              console.log("Data: " + data + "\nStatus: " + status);
-      });
+  // The red button click functions run asynchronously in the browser
+  $('#red_led_btn').click(function() {
+    if (led1 === "OFF") {
+      led1 = "ON";
+    } else {
+      led1 = "OFF";
+    }
+    var params = 'led=1&state=' + led1;
+    console.log('Led Command with params:' + params);
+    $.post('/ledcmd', params, function(data, status) {
+      console.log("Data: " + data + "\nStatus: " + status);
     });
-    // The green button click functions run asynchronously in the browser
-    $('#grn_led_btn').click(function() {
-      if (led2 === "OFF") {led2 = "ON";} else {led2 = "OFF";}
-      var params = 'led=2&state='+led2;
-      console.log('Led Command with params:' + params);
-      $.post('/ledcmd', params, function(data, status){
-              console.log("Data: " + data + "\nStatus: " + status);
-      });
-    });
-
-    // The blue button click functions run asynchronously in the browser
-    $('#blu_led_btn').click(function() {
-      if (led3 === "OFF") {led3 = "ON";} else {led3 = "OFF";}
-      var params = 'led=3&state='+led3;
-      console.log('Led Command with params:' + params);
-      $.post('/ledcmd', params, function(data, status){
-              console.log("Data: " + data + "\nStatus: " + status);
-      });
-    });
-
-      MBAR_TO_inHG = 0.029529983071;
-      var data = [];
-
-      function getDateNow() {
-        var d = new Date();
-        var date = d.getMonth() + 1 + '/' + d.getDate() + '/' + (d.getFullYear() - 2000);
-        var time = d.getHours() + ':' + d.getMinutes() + ':' + d.getSeconds();
-        return (date + " " + time);
-      }
-
-      updateSensors = (function(d) {
-        var t_c = d['temperature'].reading;
-        var t_f = (t_c * 9) / 5.0 + 32;
-        var p_mbar = d['pressure'].reading;
-        var p_inHg = p_mbar * MBAR_TO_inHG;
-
-        var date_now = getDateNow();
-        var t = t_c.toFixed(1) + '|' + t_f.toFixed(1);
-        var p = p_mbar.toFixed(1) + '|' + p_inHg.toFixed(2);
-
-        var obj = {};
-        obj['date'] = date_now;
-        obj['temp'] = t;
-        obj['press'] = p;
-        data.push(obj);
-
-        if (data.length > 3) {
-          data.shift();
-          clearTable();
-          updateTable(data);
-        }
-      });
-
-      function updateTable(data) {
-        $('tr.param-row').each(function(i) {
-          var tm = '<td>' + data[i]['date'] + '</td>';
-          var temp = '<td>' + data[i]['temp'] + '</td>';
-          var press = '<td>' + data[i]['press'] + '</td>';
-          $(this).append(tm);
-          $(this).append(temp);
-          $(this).append(press);
-        });
-      }
-
-      function clearTable() {
-        $('tr.param-row').each(function(i) {
-          $(this).empty();
-        });
-      }
-
-      // setInterval is for simulating sensor data only
-    //   setInterval(updateSensors, 3000);
   });
+  // The green button click functions run asynchronously in the browser
+  $('#grn_led_btn').click(function() {
+    if (led2 === "OFF") {
+      led2 = "ON";
+    } else {
+      led2 = "OFF";
+    }
+    var params = 'led=2&state=' + led2;
+    console.log('Led Command with params:' + params);
+    $.post('/ledcmd', params, function(data, status) {
+      console.log("Data: " + data + "\nStatus: " + status);
+    });
+  });
+
+  // The blue button click functions run asynchronously in the browser
+  $('#blu_led_btn').click(function() {
+    if (led3 === "OFF") {
+      led3 = "ON";
+    } else {
+      led3 = "OFF";
+    }
+    var params = 'led=3&state=' + led3;
+    console.log('Led Command with params:' + params);
+    $.post('/ledcmd', params, function(data, status) {
+      console.log("Data: " + data + "\nStatus: " + status);
+    });
+  });
+
+  MBAR_TO_inHG = 0.029529983071;
+  var data = [];
+
+  // from http://stackoverflow.com/questions/2998784/how-to-output-integers-with-leading-zeros-in-javascript
+  function zeropad(num, size) {
+    var s = "000000000" + num;
+    return s.substr(s.length - size);
+  }
+
+  function getDateNow() {
+    var d = new Date();
+    var date = (d.getFullYear()) + '-' + d.getMonth() + 1 + '-' + d.getDate();
+    var time = zeropad(d.getHours(), 2) + ':' + zeropad(d.getMinutes(), 2) + ':' + zeropad(d.getSeconds(), 2);
+    return {
+      epoch: time,
+      date: (date + " " + time)
+    };
+  }
+
+  updateSensors = (function(d) {
+    var t_c = d['temperature'].reading;
+    var p_mbar = d['pressure'].reading;
+    var t_f = (t_c * 9) / 5.0 + 32.0;
+    var p_inHg = p_mbar * MBAR_TO_inHG;
+
+    var timedata = getDateNow();
+    var t = t_c.toFixed(1) + '|' + t_f.toFixed(1);
+    var p = p_mbar.toFixed(1) + '|' + p_inHg.toFixed(2);
+
+    var obj = {};
+    obj['date'] = timedata.date;
+    obj['time'] = timedata.date;
+    obj['temp'] = t;
+    obj['press'] = p;
+    data.push(obj);
+
+    console.log(timedata);
+    if (data.length > 5) {
+      data.shift();
+      clearTable();
+      updateTable(data);
+      update_temp_chart(data);
+      update_press_chart(data);
+    }
+  });
+
+  function updateTable(data) {
+    $('tr.param-row').each(function(i) {
+      var tm = '<td>' + data[i]['date'] + '</td>';
+      var temp = '<td>' + data[i]['temp'] + '</td>';
+      var press = '<td>' + data[i]['press'] + '</td>';
+      $(this).append(tm);
+      $(this).append(temp);
+      $(this).append(press);
+    });
+  }
+
+  function clearTable() {
+    $('tr.param-row').each(function(i) {
+      $(this).empty();
+    });
+  }
+
+  var tempGraph = new Morris.Line({
+    element: 'mytempchart',
+    data: [
+      // { time: '00:00:01', value: 0 },
+      // { time: '00:00:02', value: 0 },
+      // { time: '00:00:03', value: 0 },
+      // { time: '00:00:04', value: 0 },
+      // { time: '00:00:05', value: 0 }
+    ],
+    xkey: 'time',
+    ykeys: ['value'],
+    labels: ['Value']
+  });
+
+  function update_temp_chart(data) {
+    var chart_data = [
+        {time: data[0]['time'], value: data[0]['temp']},
+        {time: data[1]['time'], value: data[1]['temp']},
+        {time: data[2]['time'], value: data[2]['temp']},
+        {time: data[3]['time'], value: data[3]['temp']},
+        {time: data[4]['time'], value: data[4]['temp']}
+    ];
+    tempGraph.setData(chart_data);
+  }
+
+  var pressGraph = new Morris.Line({
+    element: 'mypresschart',
+    data: [
+    ],
+    xkey: 'press',
+    ykeys: ['value'],
+    labels: ['Value']
+  });
+
+  function update_press_chart(data) {
+    var chart_data = [
+        {press: data[0]['time'], value: data[0]['press']},
+        {press: data[1]['time'], value: data[1]['press']},
+        {press: data[2]['time'], value: data[2]['press']},
+        {press: data[3]['time'], value: data[3]['press']},
+        {press: data[4]['time'], value: data[4]['press']}
+    ];
+    pressGraph.setData(chart_data);
+  }
+
+});
