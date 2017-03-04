49fa466a5f824cce95050f2ac23d947ac0bd3759
R. Hill <rhill@uw.edu>
2017-03-04 11:42:41 -0800

    working mqtt_client_test.py

--- labs/lab8/webapp/mqtt_client_test.py
+++ labs/lab8/webapp/mqtt_client_test.py
@@ -4,13 +4,13 @@ import socket
 import paho.mqtt.client as paho
 from sense import PiSenseHat
 
-myHostname = 'iot405b.local'
+myHostname = 'iot405b'
 
-localBroker = "iot405b.local"     # Local MQTT broker
+localBroker = "iot405b"     # Local MQTT broker
 localPort   = 1883          # Local MQTT port
 localUser   = "pi"          # Local MQTT user
 localPass = "raspberry"     # Local MQTT password
-localTopic = "iot/sensor"  # Local MQTT topic to monitor
+localTopic = "iot/sensor"   # Local MQTT topic to monitor
 localTimeOut = 120          # Local MQTT session timeout
 
 # The callback for when a CONNACK message is received from the broker.
@@ -19,7 +19,7 @@ def on_connect(client, userdata, flags, rc):
 
 # The callback for when a PUBLISH message is received from the broker.
 def on_message(client, userdata, msg):
-    print (string.split(msg.payload))
+    print ("on_message: " + string.split(msg.payload))
 
 # display one row of blue LEDs (just for fun)
 def displayLine(row):
@@ -42,14 +42,13 @@ if socket.gethostname().find('.') >= 0:
     hostname=socket.gethostname()
 else:
     hostname=socket.gethostbyaddr(socket.gethostname())[0]
-print('hostname: ' + hostname)
-hostname = hostname + '.local'
-print('new hostname: ' + hostname)
 
 # Setup to Publish Sensor Data
 mqttc = paho.Client()
 mqttc.on_connect = on_connect
 mqttc.on_message = on_message
+mqttc.username_pw_set(localUser, localPass)
+
 mqttc.connect(localBroker, localPort, localTimeOut)
 
 # initialize message dictionary
@@ -58,7 +57,7 @@ msg = {'topic':localTopic, 'payload':"", 'qos':0, 'retain':False }
 pi_sense.clear_display()
 # loop
 print('Getting Sensor Data')
-for i in range(1,4):
+for i in range(1,9):
     print("SensorSet[%d]" % (i))
     displayLine(i-1)
     sensors = pi_sense.getAllSensors()
@@ -68,7 +67,7 @@ for i in range(1,4):
     print("msg["+str(i)+"]:"+msg['payload'])
 
 #   publish(topic, payload=None, qos=0, retain=False)
-    mqttc.publish('iot/test', msg['payload'], 0)
+    mqttc.publish('iot/test', msg['payload'], 1)
     time.sleep(2.0)
 
 print('End of MQTT Messages')
